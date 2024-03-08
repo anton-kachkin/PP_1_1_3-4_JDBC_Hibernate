9f40bfd8905866d8130da5d07d48cbc22cfa4f7b
anton-kachkin <ka4kin.anton@yandex.ru>
2024-03-08 20:23:48 +0300

    v1

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,34 +1,48 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao;
 
+    public UserServiceImpl() {
+        SessionFactory sessionFactory = Util.getSessionFactory();
+        this.userDao = new UserDaoHibernateImpl(sessionFactory);
+    }
+
+    @Override
     public void createUsersTable() {
         userDao.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         userDao.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         userDao.saveUser(name, lastName, age);
         System.out.println("Юзер с именем - " + name + " добавлен в базу данных");
     }
 
+    @Override
     public void removeUserById(long id) {
         userDao.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         userDao.cleanUsersTable();
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,36 +3,86 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
-    static Connection connection;
+//    private static final String URL = "jdbc:mysql://localhost:3306/testdb";
+//    private static final String USERNAME = "root";
+//    private static final String PASSWORD = "root";
+//    static Connection connection;
 
-    public static Connection getConnection() {
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Connected to the database.");
+//    public static Connection getConnection() {
+//        try {
+//            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+//            if (!connection.isClosed()) {
+//                System.out.println("Connected to the database.");
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("Failed to connect to the database.");
+//            e.printStackTrace();
+//        }
+//        return connection;
+//    }
+
+
+    //    public static void closeConnection() {
+//        if (connection != null) {
+//            try {
+//                connection.close();
+//                System.out.println("Connection closed.");
+//            } catch (SQLException e) {
+//                System.out.println("Failed to close connection.");
+//            }
+//        }
+//    }
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testdb?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "create");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.out.println("Failed to create SessionFactory.");
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to the database.");
-            e.printStackTrace();
         }
-        return connection;
+        return sessionFactory;
     }
 
-
-    public static void closeConnection() {
-        if (connection != null) {
-            try {
-                connection.close();
-                System.out.println("Connection closed.");
-            } catch (SQLException e) {
-                System.out.println("Failed to close connection.");
-            }
+    public static void closeSessionFactory() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+            System.out.println("SessionFactory closed.");
         }
     }
 }
